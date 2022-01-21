3008dc92c456f51217a80477cdb6b2d86e30ef08
Alexander <gewald.alexander@sol.at>
2022-01-21 13:51:33 +0100

    GenNumber generator for split entry and exit

--- genNumbers_PersonEntrance.py
+++ genNumbers_PersonEntrance.py
@@ -5,22 +5,31 @@ def genNumbers():
     #time difference of person is min 5 minutes and max 1 hour
 
     startingpoint = 1594512095 #start of sensor data
-    endpoint = 1595203418 #end of sensor data
+    endpoint = 1595203418 #end of sensor datax
 
-    values = []  # declare empty list
+    values_entry = [] # declare empty list
+
     for i in range(20): #20 persons enter room
-        temp = random.randint(startingpoint, endpoint)
-        temp1 = temp + random.randint(300, 3600) #person leaves room after random time between 5-60 minutes
-        values.append(temp)  # insert rand int into list
-        values.append(temp1) #insert exit time
+        entrytime = random.randint(startingpoint, endpoint)
+        values_entry.append(entrytime)
 
-    values.sort() #sort array for easier processing
-    with open('C:/Users/Franz/OneDrive/Desktop/BAC1/datensatz/persondata.csv', 'w') as f:
-        f.write("timestamp\n") #add heading to csv
-        for line in values:
-            f.write(str(line))  #write one value
-            f.write('\n')   #newline to get new column in csv
+    values_entry.sort()
+    values_exit = []
+
+    #create times for exit
+    for i in range(20):
+        exittime = values_entry[i] + random.randint(300, 3600)  # person leaves room after random time between 5-60 minutes
+        values_exit.append(exittime)
+
+    values_entry.insert(0, "entry")
+    values_exit.insert(0, "exit")
+
+
+    f = open('persondata.csv', 'w')
+    for i in range(20):
+        f.write(str(values_entry[i]) + ", " + str(values_exit[i]))  # write one value
+        f.write('\n')  # newline to get new column in csv
 
     f.close()
 
-    return values
+    return True
