8e507ee867824330125d60b4665db78234233d79
Alexander <gewald.alexander@sol.at>
2022-01-21 13:49:10 +0100

    Added Application Context

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-
 from genNumbers_PersonEntrance import *
 from csv_interface import *
 from application import *
@@ -10,21 +9,31 @@ import csv_interface
 def main():
     user_input = input("Time in Format: YYYY-MM-DD HH:mm:ss")
     time = parse(user_input).timestamp()  # time is in unixformat
-    our_app = AssetApplication(time)
-    #genNumbers()
-    temp = genereateRange(time)
+    # genNumbers()
+    temp = generateRange(time)
     lpg = genlpgMEAN(temp)
     smoke = genSmokeMEAN(temp)
     temperatur = genTempMEAN(temp)
 
     temperaturArr = temp['temp']
     timeArr = temp['ts']
-    myTime = datetime(time)
+
+    # check for persons
+    person_timestamp = person_check.checkForPersons(time)
+    # print for debugging purposes
+    print(person_timestamp)
+
+    context = QApplication([])
+    context.setStyle('Fusion')
+    our_app = AssetApplication(user_input)
+    our_app.show()
+    context.exec()
+    # myTime = datetime(time)
+    # print for debugging purposes
     print(lpg, smoke, temperatur)
 
 
 
 if __name__ == "__main__":
     main()
-    #values = genNumbers()
     # print(values)
